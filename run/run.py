eb580712ce8d87f3a984c7de352c73284efdb80f
Andrew Shen <aashen@ucsd.edu>
2024-01-24 10:12:27 -0600

    configs

--- run/run.py
+++ run/run.py
@@ -1,6 +1,6 @@
 # Script to train the end-to-end decoder
 
-# Run script using command "python3 run/run.py" in home directory
+# Run script using command "python3 run/run.py configs/configs_cage.yaml" in home directory
 import sys
 import wandb
 import pytorch_lightning as pl
@@ -11,39 +11,31 @@ sys.path.append('/Users/andrewshen/Desktop/neural_decoding')
 from data.data import *
 from model.model import *
 from TrainingModule import *
+from utils.constants import *
 
 if __name__ == "__main__":
     print("Running 'run.py'...")
-    
-    # Define parameters
-    T = 5000
-    N = 10
-    M = 3
-    d = 3  # num_modes
-    b = 8
-    type = "emg"
-    epochs = 500
-    lr = 0.0001
-    record = True
-    m1_path = "data/set2_data/m1_set2.npy"
-    emg_path = "data/set2_data/emg_set2.npy"
-    behavioral_path = "data/set2_data/behavioral_set2.npy"
+
+    # Load configs
+    config = load_config()
 
     # Load in dataset
-    dataset = Cage_Dataset(m1_path=m1_path, emg_path=emg_path, behavioral_path=behavioral_path, num_modes=d, batch_size=b, dataset_type=type)
+    dataset = Cage_Dataset(m1_path=config.m1_path, emg_path=config.emg_path, 
+                           behavioral_path=config.behavioral_path, num_modes=config.d, 
+                           batch_size=config.b, dataset_type=config.type)
     # dataset = M1_EMG_Dataset_Toy(num_samples=T, num_neurons=N, num_muscles=M, num_modes=d, batch_size=b, dataset_type=type)
 
     # Define model
     # model = CombinedModel(N, M, d)
-    model = CombinedModel(dataset.N, dataset.M, d)
-    model = TrainingModule(model, lr, record, type)
+    model = CombinedModel(dataset.N, dataset.M, config.d)
+    model = TrainingModule(model, config.lr, config.record, config.type)
 
     # Define trainer
-    if record:
+    if config.record:
         wandb_logger = WandbLogger(project="neural_decoding")
-        trainer = Trainer(max_epochs=epochs, logger=wandb_logger, callbacks=Callback(dataset))
+        trainer = Trainer(max_epochs=config.epochs, logger=wandb_logger, callbacks=Callback(dataset))
     else:
-        trainer = Trainer(max_epochs=epochs, callbacks=Callback(dataset))
+        trainer = Trainer(max_epochs=config.epochs, callbacks=Callback(dataset))
 
     # Fit the model
     trainer.fit(model, train_dataloaders=dataset.train_dataloader(), val_dataloaders=dataset.val_dataloader())
---
+++ utils/constants.py
@@ -0,0 +1,39 @@
+# Script for background processes
+
+# Import packages
+import sys
+import yaml
+
+class Config():
+
+    def __init__(self, args):
+        self.T = args["T"]
+        self.N = args["N"]
+        self.M = args["M"]
+        self.d = args["d"]  # num_modes
+        self.b = args["b"]
+        self.type = args["type"]
+        self.epochs = args["epochs"]
+        self.lr = args["lr"]
+        self.record = args["record"]
+        self.m1_path = args["m1_path"]
+        self.emg_path = args["emg_path"]
+        self.behavioral_path = args["behavioral_path"]
+
+
+def load_config():
+    """
+    Loads configuration file and reads values for usage.
+    Input: None
+    Output: (Config) config object
+    """
+
+    try:
+        config_path = sys.argv[1]
+    except Exception:
+        raise Exception("Must include config file to run.")
+
+    args = yaml.safe_load(open(config_path, "r"))
+
+    return Config(args)
+
