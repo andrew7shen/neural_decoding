b1674165c3e215ee3a3b4b9ef3ae1eba87b37a10
Andrew Shen <aashen@ucsd.edu>
2024-05-09 11:35:23 -0500

    b=10 manual decoder weights with regularization experiments

--- jobscript_run.sh
+++ jobscript_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p31796
 #SBATCH -p normal
-#SBATCH -t 1-00:00
+#SBATCH -t 2-00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
 #SBATCH --output=training_out.txt
@@ -11,5 +11,11 @@ source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
 
-python3 run/run.py configs/b_configs/configs_cage_t100_b10.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.5.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.1.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.01.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.5_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.1_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.01_2.yaml
