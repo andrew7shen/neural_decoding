12c16afb549271fa1ead6b7ed64e0c650a139354
Andrew Shen <aashen@ucsd.edu>
2024-05-18 22:19:22 -0400

    re-run b=10 nonlinear experiments with regularization after layer initialization fix

--- jobscript_run.sh
+++ jobscript_run.sh
@@ -11,7 +11,14 @@ source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
 
-python3 run/run.py configs/t100_configs/configs_cage_t100.yaml
-python3 run/run.py configs/t100_configs/configs_cage_t100_2.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_2.yaml
+# python3 run/run.py configs/t100_configs/configs_cage_t100.yaml
+# python3 run/run.py configs/t100_configs/configs_cage_t100_2.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_2.yaml
+
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.04.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.03.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.04_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.03_2.yaml
