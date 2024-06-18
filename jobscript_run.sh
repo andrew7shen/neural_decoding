182eaae21f789104fe1c481575e335182b1b0e48
Andrew Shen <andrewshen@andrews-air-9.mynetworksettings.com>
2024-06-17 21:04:41 -0400

    more b=10 linear and nonlinear runs with regularization

--- jobscript_run.sh
+++ jobscript_run.sh
@@ -16,20 +16,28 @@ cd neural_decoding/
 # python3 run/run.py configs/b_configs/configs_cage_t100_b10.yaml
 # python3 run/run.py configs/b_configs/configs_cage_t100_b10_2.yaml
 
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.1.yaml
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.05.yaml
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.025.yaml
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.01.yaml
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.1_2.yaml
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.05_2.yaml
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.025_2.yaml
-python3 run/run.py configs/w_configs/configs_cage_t100_w0.01_2.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.1.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.025.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.01.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.1_2.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05_2.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.025_2.yaml
-python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.01_2.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.1.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.05.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.025.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.01.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.1_2.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.05_2.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.025_2.yaml
+# python3 run/run.py configs/w_configs/configs_cage_t100_w0.01_2.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.1.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.025.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.01.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.1_2.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.05_2.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.025_2.yaml
+# python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.01_2.yaml
 
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.6.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.4.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.15.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.6_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.4_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.2_2.yaml
+python3 run/run.py configs/b_configs/configs_cage_t100_b10_w0.15_2.yaml
