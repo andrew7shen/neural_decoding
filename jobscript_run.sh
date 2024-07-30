cf774ce282efb3a84ed1b5d0cc4a0b09474248eb
Andrew Shen <andrew7shen@gmail.com>
2024-07-29 23:00:18 -0400

    set1 baseline model sanity check

--- jobscript_run.sh
+++ jobscript_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p31796
 #SBATCH -p normal
-#SBATCH -t 1-00:00
+#SBATCH -t 3:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
 #SBATCH --output=training_out.txt
@@ -11,12 +11,12 @@ source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
 
-python3 run/run.py configs/robust_configs/configs_cage_t100_relu0.1.yaml
-python3 run/run.py configs/robust_configs/configs_cage_t100_relu0.01.yaml
-python3 run/run.py configs/robust_configs/configs_cage_t100_onlyrelu0.01.yaml
-python3 run/run.py configs/robust_configs/configs_cage_t100_onlytanh.yaml
+# python3 run/run.py configs/robust_configs/configs_cage_t100_relu0.1.yaml
+# python3 run/run.py configs/robust_configs/configs_cage_t100_relu0.01.yaml
+# python3 run/run.py configs/robust_configs/configs_cage_t100_onlyrelu0.01.yaml
+# python3 run/run.py configs/robust_configs/configs_cage_t100_onlytanh.yaml
 
-# python3 run/run.py configs/t100_configs/configs_cage_t100_set1.yaml
+python3 run/run.py configs/t100_configs/configs_cage_t100_set1.yaml
 
 # python3 run/run.py configs/t100_configs/configs_cage_t100.yaml
 # python3 run/run.py configs/t100_configs/configs_cage_t100_2.yaml
