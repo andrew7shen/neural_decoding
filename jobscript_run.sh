7ab4ef0e277511be31da05f6a2dcef9cddee01fb
Andrew Shen <andrew7shen@gmail.com>
2024-09-11 12:59:04 -0500

    small change

--- jobscript_run.sh
+++ jobscript_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p31796
 #SBATCH -p normal
-#SBATCH -t 12:00:00
+#SBATCH -t 3:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
 #SBATCH --output=training_out.txt
@@ -10,6 +10,8 @@
 source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
+
+python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming.yaml
 
 # python3 run/run.py configs/robust_configs/configs_cage_t100_relu0.1.yaml
 # python3 run/run.py configs/robust_configs/configs_cage_t100_relu0.01.yaml
@@ -22,15 +24,15 @@ cd neural_decoding/
 # python3 run/run.py configs/s_configs/configs_cage_t100_set1_s45.yaml
 # python3 run/run.py configs/s_configs/configs_cage_t100_set1_s46.yaml
 
-python3 run/run.py configs/d_configs/configs_cage_t100_set1_d3.yaml
-python3 run/run.py configs/d_configs/configs_cage_t100_set1_d4.yaml
-python3 run/run.py configs/d_configs/configs_cage_t100_set1_d5.yaml
-python3 run/run.py configs/d_configs/configs_cage_t100_set1_d6.yaml
-python3 run/run.py configs/d_configs/configs_cage_t100_set1_d7.yaml
+# python3 run/run.py configs/d_configs/configs_cage_t100_set1_d3.yaml
+# python3 run/run.py configs/d_configs/configs_cage_t100_set1_d4.yaml
+# python3 run/run.py configs/d_configs/configs_cage_t100_set1_d5.yaml
+# python3 run/run.py configs/d_configs/configs_cage_t100_set1_d6.yaml
+# python3 run/run.py configs/d_configs/configs_cage_t100_set1_d7.yaml
 
 # python3 run/run.py configs/t100_configs/configs_cage_t100_set1.yaml
-python3 run/run.py configs/t100_configs/configs_cage_t100_none.yaml
-python3 run/run.py configs/d_configs/configs_cage_t100_none_d6.yaml
+# python3 run/run.py configs/t100_configs/configs_cage_t100_none.yaml
+# python3 run/run.py configs/d_configs/configs_cage_t100_none_d6.yaml
 
 
 # python3 run/run.py configs/t100_configs/configs_cage_t100.yaml
