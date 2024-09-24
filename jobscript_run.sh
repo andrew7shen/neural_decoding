056f224858786af0a12a20a58c252323d45af81a
Andrew Shen <andrew7shen@gmail.com>
2024-09-23 22:46:52 -0500

    small fix

--- jobscript_run.sh
+++ jobscript_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p31796
 #SBATCH -p normal
-#SBATCH -t 1-00:00
+#SBATCH -t 6:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
 #SBATCH --output=training_out.txt
@@ -23,14 +23,14 @@ cd neural_decoding/
 # python3 run/run.py configs/t100_configs/configs_cage_t100_set1_kmeansweights.yaml
 # python3 run/run.py configs/t100_configs/configs_cage_t100_set1_randomweights.yaml
 
-python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming.yaml
-python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming_sitting_still.yaml
-python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_unlabeled.yaml
-python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_labeled.yaml
+# python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming.yaml
+# python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming_sitting_still.yaml
+# python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_unlabeled.yaml
+# python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_labeled.yaml
 
-python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming_d6.yaml
-python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming_sitting_still_d6.yaml
-python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_unlabeled_d6.yaml
+# python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming_d6.yaml
+# python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_grooming_sitting_still_d6.yaml
+# python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_unlabeled_d6.yaml
 python3 run/run.py configs/generalizability_configs/configs_cage_t100_set1_generalizability_labeled_d6.yaml
 
 # python3 run/run.py configs/mouse_configs/configs_mouse.yaml
