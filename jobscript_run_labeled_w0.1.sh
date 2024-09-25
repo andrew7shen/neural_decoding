e95ffa23cd0e540882b08f94f19dafb1b4b00d5b
Andrew Shen <andrew7shen@gmail.com>
2024-09-25 10:25:40 -0500

    generalizability experiments sweep regularization for unlabeled and labeled

---
+++ jobscript_run_labeled_w0.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A p31796
+#SBATCH -p normal
+#SBATCH -t 6:00:00
+#SBATCH -N 1
+#SBATCH --mem=30G
+#SBATCH --output=training_out.txt
+#SBATCH --error=training_err.txt
+
+source quest_decoding_venv/bin/activate
+module load python/3.9.16
+cd neural_decoding/
+
+python3 run/run.py configs/generalizability_configs/regularization/labeled_w0.1.yaml
