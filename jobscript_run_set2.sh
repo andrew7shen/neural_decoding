002fea95d675d0a5e90b3712ca6936d57c8b6a8b
Andrew Shen <andrew7shen@gmail.com>
2024-10-16 19:58:07 -0500

    temperature experiments boost performance cont

--- jobscript_run_set2.sh
+++ jobscript_run_set2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p31796
 #SBATCH -p normal
-#SBATCH -t 12:00:00
+#SBATCH -t 6:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
 #SBATCH --output=training_out.txt
@@ -11,9 +11,14 @@ source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
 
-python3 run/run.py configs/t100_configs/configs_cage_t100_set2.yaml
-python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp1.0.yaml
-python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp5.0.yaml
-python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp10.0.yaml
-python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0.yaml
-python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp100.0.yaml
+# python3 run/run.py configs/t100_configs/configs_cage_t100_set2.yaml
+# python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp1.0.yaml
+# python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp5.0.yaml
+# python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp10.0.yaml
+# python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0.yaml
+# python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp100.0.yaml
+
+python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0_850epochs_flatten.yaml
+python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0_1000epochs_flatten.yaml
+python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0_1250epochs_flatten.yaml
+python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0_1500epochs_flatten.yaml
