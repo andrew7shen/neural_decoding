313e86021fe767527ecb60a9cab42bfb8b66eb9a
Andrew Shen <andrew7shen@gmail.com>
2024-10-21 16:23:07 -0500

    mouse dataset more modes experiments boost performance

---
+++ jobscript_run_mouse_d11.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A p31796
+#SBATCH -p normal
+#SBATCH -t 12:00:00
+#SBATCH -N 1
+#SBATCH --mem=30G
+#SBATCH --output=training_mouse_d11_out.txt
+#SBATCH --error=training_mouse_d11_err.txt
+
+source quest_decoding_venv/bin/activate
+module load python/3.9.16
+cd neural_decoding/
+
+python3 run/run.py configs/mouse_configs/configs_mouse_temp50.0_d11.yaml
