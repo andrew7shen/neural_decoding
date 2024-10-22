3f69d23b7666d6508217ad8419e0d5307f6e361a
Andrew Shen <andrew7shen@gmail.com>
2024-10-22 01:20:49 -0500

    small change

--- jobscript_run_mouse_1250epochs.sh
+++ jobscript_run_mouse_1250epochs.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 12:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
-#SBATCH --output=training_mouse_1250epochs_out.txt
-#SBATCH --error=training_mouse_1250epochs_err.txt
+#SBATCH --output=training_mouse_1250epochs_d15_out.txt
+#SBATCH --error=training_mouse_1250epochs_d15_err.txt
 
 source quest_decoding_venv/bin/activate
 module load python/3.9.16
--- jobscript_run_mouse_750epochs.sh
+++ jobscript_run_mouse_750epochs.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 8:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
-#SBATCH --output=training_mouse_750epochs_out.txt
-#SBATCH --error=training_mouse_750epochs_err.txt
+#SBATCH --output=training_mouse_750epochs_d15_out.txt
+#SBATCH --error=training_mouse_750epochs_d15_err.txt
 
 source quest_decoding_venv/bin/activate
 module load python/3.9.16
