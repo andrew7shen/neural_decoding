4d9a6ffd2ac0ead9654f64a22b97abd2c2250f0b
Andrew Shen <andrew7shen@gmail.com>
2024-10-22 01:28:44 -0500

    mouse dataset temperature experiments boost performance more modes cont

--- jobscript_run_mouse_d20.sh
+++ jobscript_run_mouse_d20.sh
@@ -4,11 +4,11 @@
 #SBATCH -t 12:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
-#SBATCH --output=training_mouse_d8_out.txt
-#SBATCH --error=training_mouse_d8_err.txt
+#SBATCH --output=training_mouse_d20_out.txt
+#SBATCH --error=training_mouse_d20_err.txt
 
 source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
 
-python3 run/run.py configs/mouse_configs/configs_mouse_temp50.0_d8.yaml
+python3 run/run.py configs/mouse_configs/configs_mouse_temp50.0_d20.yaml
