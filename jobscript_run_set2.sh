7875297fb7eb2ec41886073e7e8a8668b8156299
Andrew Shen <andrew7shen@gmail.com>
2024-10-23 12:39:58 -0500

    monkey dataset rollback experiment

--- jobscript_run_set2.sh
+++ jobscript_run_set2.sh
@@ -11,10 +11,12 @@ source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
 
+
+python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0.yaml
 # python3 run/run.py configs/t100_configs/configs_cage_t100_set2_d6.yaml
 # python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0_750epochs_flatten_d6.yaml
 
-python3 run/run.py configs/t100_configs/configs_cage_t100_set2.yaml
+# python3 run/run.py configs/t100_configs/configs_cage_t100_set2.yaml
 # python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp1.0.yaml
 # python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp5.0.yaml
 # python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp10.0.yaml
