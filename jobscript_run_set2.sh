c35f982b390f64f59a92e484c3f7fc0b873a1379
Andrew Shen <andrew7shen@gmail.com>
2025-02-03 18:55:04 -0500

    l1 regularization on final outputs sweep lambda

--- jobscript_run_set2.sh
+++ jobscript_run_set2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p31796
 #SBATCH -p normal
-#SBATCH -t 3:00:00
+#SBATCH -t 5:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
 #SBATCH --output=training_out.txt
@@ -11,8 +11,11 @@ source quest_decoding_venv/bin/activate
 module load python/3.9.16
 cd neural_decoding/
 
-python3 run/run.py configs/t100_configs/configs_cage_t100_set2.yaml
-python3 run/run.py configs/t100_configs/configs_cage_t100_set2_l1.yaml
+# python3 run/run.py configs/t100_configs/configs_cage_t100_set2.yaml
+# python3 run/run.py configs/t100_configs/configs_cage_t100_set2_l1.yaml
+python3 run/run.py configs/t100_configs/configs_cage_t100_set2_l1_0.05.yaml
+python3 run/run.py configs/t100_configs/configs_cage_t100_set2_l1_0.1.yaml
+python3 run/run.py configs/t100_configs/configs_cage_t100_set2_l1_0.5.yaml
 # python3 run/run.py configs/t100_configs/configs_cage_t100_set2_globalbias.yaml
 
 # python3 run/run.py configs/temp_configs/configs_cage_t100_set2_temp50.0_lambda0.yaml
