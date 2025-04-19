9f96b3c1e180eb1ce2674bb6790161a354e9a18d
Andrew Shen <andrew7shen@gmail.com>
2025-04-19 16:18:20 -0400

    try other non-linear clustering with single and multiple ffnns on monkey dataset more time

--- jobscript_run_set2.sh
+++ jobscript_run_set2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -A p31796
 #SBATCH -p normal
-#SBATCH -t 3:00:00
+#SBATCH -t 12:00:00
 #SBATCH -N 1
 #SBATCH --mem=30G
-#SBATCH --output=training_out.txt
-#SBATCH --error=training_err.txt
+#SBATCH --output=training_set2_out.txt
+#SBATCH --error=training_set2_err.txt
 
 source quest_decoding_venv/bin/activate
 module load python/3.9.16
