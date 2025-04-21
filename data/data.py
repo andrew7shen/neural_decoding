509cb2faf9f34a595c40f35fa5e7678461edffd5
Andrew Shen <andrew7shen@gmail.com>
2025-04-21 19:47:03 -0400

    train/val/test analysis on default monkey dataset 80/10/10 split sanity check

--- data/data.py
+++ data/data.py
@@ -557,14 +557,15 @@ class Cage_Dataset(pl.LightningDataModule):
                     X_eval = X_val
                     y_eval = y_val
                 else:
-                    # X_train, X_val_test, y_train, y_val_test = train_test_split(m1, labels, test_size=0.2, random_state=42)
-                    # X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
+                    # TODO: Try 80/10/10 split
+                    X_train, X_val_test, y_train, y_val_test = train_test_split(m1, labels, test_size=0.2, random_state=42)
+                    X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
                     # TODO: Try 60/20/20 split
                     # X_train, X_val_test, y_train, y_val_test = train_test_split(m1, labels, test_size=0.4, random_state=42)
                     # X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
                     # TODO: Try 70/15/15 split
-                    X_train, X_val_test, y_train, y_val_test = train_test_split(m1, labels, test_size=0.3, random_state=42)
-                    X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
+                    # X_train, X_val_test, y_train, y_val_test = train_test_split(m1, labels, test_size=0.3, random_state=42)
+                    # X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
                     if self.eval_dataset == "val":
                         X_eval = X_val
                         y_eval = y_val
